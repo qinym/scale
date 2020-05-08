ec45bcf61834cf4333c6361057da8ca523374bea
qin.ym <1158722289@qq.com>
2020-05-08 14:40:53 +0800

    动态汽车衡项目框架搭建

---
+++ src/main/java/com/dynamic/truck/scale/consumer/Demo01Consumer.java
@@ -0,0 +1,24 @@
+package com.dynamic.truck.scale.consumer;
+
+import com.dynamic.truck.scale.message.Demo01Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo01Consumer {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @JmsListener(destination = Demo01Message.QUEUE)
+    public void onMessage(Demo01Message message) {
+        logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+    }
+
+//    @JmsListener(destination = Demo01Message.QUEUE)
+//    public void onMessage(javax.jms.Message message) {
+//        logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+//    }
+
+}
---
+++ src/test/java/com/dynamic/truck/scale/ScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.truck.scale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
